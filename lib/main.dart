ccd2f21e69ec50510bebc5545c34f5db909bd435
unknown <uper890@gmail.com>
2021-05-14 00:54:02 -0500

    navegacion

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:navegacion/src/pages/alert_page.dart';
+import 'package:navegacion/src/pages/avatar_page.dart';
+import 'package:navegacion/src/pages/card_page.dart';
+import 'package:navegacion/src/pages/home_page.dart';
+import 'src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'proyecto_15751',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => HomePage(),
+        'alert': (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage(),
+        'cars': (BuildContext context) => CardPage(),
+      },
+    );
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,73 @@
+import 'package:navegacion/src/providers/menu_provider.dart';
+import 'package:navegacion/src/utils/icono_string_util.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('****Navegación****'),
+        backgroundColor: Colors.yellow,
+      ),
+      body: _lista(context),
+    );
+  }
+
+  Widget _lista(BuildContext context) {
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      builder: (context, snapshot) {
+        print(snapshot.data);
+        return ListView(children: _listaItems(snapshot.data, context));
+      },
+    );
+  }
+
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: Icon(
+          Icons.keyboard_arrow_right_sharp,
+          color: Colors.black,
+        ),
+        onTap: () {
+          /*   context: context,
+          showDialog(
+            builder: (BuildContext context) => _buildPopUpDialog(context, opt['texto']),
+        },
+          );*/
+          Navigator.pushNamed(context, opt['ruta']);
+        },
+      );
+      opciones..add(widgetTemp)..add(Divider());
+    });
+    return opciones;
+  }
+
+  Widget _buildPopUpDialog(BuildContext context, String opt) {
+    return new AlertDialog(
+      title: const Text('Me diste click'),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Hola soy la opcion ' + opt),
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          textColor: Theme.of(context).primaryColor,
+          child: const Text('Cerrar'),
+        ),
+      ],
+    );
+  }
+}
